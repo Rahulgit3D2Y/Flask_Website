4b6c5931f70948d8f148b3e71615250f05bde8ee
Rahulgit3D2Y <rc2504000@gmail.com>
2024-08-31 11:00:52 +0530

    done

--- frontend/src/ContactList.jsx
+++ frontend/src/ContactList.jsx
@@ -1,16 +1,36 @@
 import React from "react";
-import PropTypes from "prop-types";
 
-const ContactsList = ({ contacts }) => {
+const ContactList = ({ contacts, updateContact, updateCallback }) => {
+  const onDelete = async (id) => {
+    try {
+      const options = {
+        method: "DELETE",
+      };
+      const response = await fetch(
+        `http://127.0.0.1:5000/delete_contact/${id}`,
+        options
+      );
+      if (response.status === 200) {
+        updateCallback();
+      } else {
+        console.error("Failed to delete");
+      }
+    } catch (error) {
+      alert(error);
+    }
+  };
+
   return (
-    <div>
+    <div className="main">
       <h2>Contacts</h2>
       <table>
         <thead>
-          <th>FirstName</th>
-          <th>lastName</th>
-          <th>Email</th>
-          <th>Actions</th>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Actions</th>
+          </tr>
         </thead>
         <tbody>
           {contacts.map((contact) => (
@@ -19,8 +39,8 @@ const ContactsList = ({ contacts }) => {
               <td>{contact.lastName}</td>
               <td>{contact.email}</td>
               <td>
-                <button>UPDATE</button>
-                <button>DELETE</button>
+                <button onClick={() => updateContact(contact)}>Update</button>
+                <button onClick={() => onDelete(contact.id)}>Delete</button>
               </td>
             </tr>
           ))}
@@ -29,14 +49,5 @@ const ContactsList = ({ contacts }) => {
     </div>
   );
 };
-ContactsList.propTypes = {
-  contacts: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      firstName: PropTypes.string.isRequired,
-      lastName: PropTypes.string.isRequired,
-      email: PropTypes.string.isRequired,
-    })
-  ).isRequired,
-};
-export default ContactsList;
+
+export default ContactList;
